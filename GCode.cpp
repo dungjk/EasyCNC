d471fddea66f6ef304eb28b3cdb24378de2d28e0
Giurlanda Francesco <franesco.giurlanda@gmail.com>
2014-07-28 14:21:25 +0200

    Renamed the classes CNCxy in CNC_Router

---
+++ GCode.cpp
@@ -0,0 +1,14 @@
+/*
+ * GCode.cpp
+ *
+ *  Created on: 26/lug/2014
+ *      Author: francescogiurlanda
+ */
+
+#include "GCode.h"
+
+GCode::GCode() {
+	// TODO Auto-generated constructor stub
+
+}
+
---
+++ GCode.h
@@ -0,0 +1,16 @@
+/*
+ * GCode.h
+ *
+ *  Created on: 26/lug/2014
+ *      Author: francescogiurlanda
+ */
+
+#ifndef GCODE_H_
+#define GCODE_H_
+
+class GCode {
+public:
+	GCode();
+};
+
+#endif /* GCODE_H_ */
