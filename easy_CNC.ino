f2b5c39a5d45324a31c8082d81ec6affbc2c2d60
Giurlanda Francesco <franesco.giurlanda@gmail.com>
2015-05-26 19:20:03 +0200

    Bug fix in easy_CNC.ino

--- easy_CNC.ino
+++ easy_CNC.ino
@@ -14,24 +14,30 @@
 #include "Position.h"
 #include "MotorDriver.h"
 
+
 #ifdef _MILLING_MACHINE
 #include "MillingMachine.h"
 #endif
 #ifdef _LASER
 #include "Laser.h"
 #endif
+#ifdef _PLOTTER_SERVO
+#include "PlotterServo.h"
+#endif
 
 #ifdef _MILLING_MACHINE
 MillingMachine tool(_MILLING_MACHINE_ENABLE_PIN, _MILLING_MACHINE_SPEED_PIN);
 #endif
-
 #ifdef _LASER
 Laser tool(_LASER_CONTROL_PIN, _LASER_CONTROL_ACTIVE_HIGH);
+#endif
+#ifdef _PLOTTER_SERVO
+PlotterServo tool(_PLOTTER_SERVO_PIN, _PLOTTER_SERVO_UP_POS, _PLOTTER_SERVO_DOWN_POS);
 #endif
 
 CNC_Router_ISR cncrt;
 
-GCode gc(&cncrt, &mill);
+GCode gc(&cncrt, &tool);
 char new_line[256];
 
 void setup(){
@@ -49,6 +55,9 @@ void setup(){
 	tool.init();
 #endif
 #ifdef _LASER
+	tool.init();
+#endif
+#ifdef _PLOTTER_SERVO
 	tool.init();
 #endif
 }
